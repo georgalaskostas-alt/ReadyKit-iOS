13d4ccf0cd49d56ab471431fd2c16a90fb7931e5
georgalaskostas-alt <georgalas.kostas@gmail.com>
2026-09-15 20:39:38 +0300

    Initial Xcode project

---
+++ ReadyKit/ReadyKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadyKitApp.swift
+//  ReadyKit
+//
+//  Created by Κωνσταντινος Γεωργαλας on 15/9/26.
+//
+
+import SwiftUI
+
+@main
+struct ReadyKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
